05eaa939260ef4347a6d8961ec86c198a8d48d5e
Beki-liv <solbek282@gmail.com>
2022-12-19 09:40:36 -0800

    add

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,1 @@
-#include "main.h"
-
-/**
-* _puts - prints a string
-* @str - string to be printed
-*/
-
-void _puts(char *str)
-{
-	int i = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(sttr[i]);
-	_putchar('\n');
-}
+#include "main.h" 
